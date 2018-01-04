509aad42b9eff5c39205884546c0c799735f99a0
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-04 21:55:34 +0530

    Move the card using Animated.View

--- src/Deck.js
+++ src/Deck.js
@@ -9,26 +9,39 @@ class Deck extends Component {
   constructor(props){
     super(props);
 
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy })
       },
       onPanResponderRelease: () => {}
     });
 
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
 
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if(index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
       return this.props.renderCard(item);
     });
   }
 
   render () {
     return (
-      <View {...this.state.panResponder.panHandlers}>
+      <View>
         {this.renderCards()}
       </View>
     );
