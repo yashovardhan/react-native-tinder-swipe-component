5f13a5f7ccd647e1b9fd6fdad94b2a07dfd030ec
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-05 16:32:24 +0530

    Add renderNoMoreCards()

--- App.js
+++ App.js
@@ -34,12 +34,28 @@ export default class App extends React.Component {
     );
   }
 
+  renderNoMoreCards() {
+    return (
+      <Card title="All Done!">
+        <Text style={{ marginBottom: 10 }}>
+          There's no more content here!
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now"
+        />
+      </Card>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Deck
           data={DATA}
           renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
           />
       </View>
     );
